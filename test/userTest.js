bddb14f295ac4bc5a9f447dbf05c06cc6482ffee
tudor96 <gltudor96@gmail.com>
2020-01-05 16:29:56 +0200

    final modifications

--- test/userTest.js
+++ test/userTest.js
@@ -7,37 +7,12 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('USER TEST REQUESTS', () => {
-
-  //   describe('/GET all events', () => {
-  //     it('it should GET all events', (done) => {
-  //       chai.request(server)
-  //         .get('/api/candidate/')
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('array');
-  //           done();
-  //         });
-  //     });
-  //     it('it should GET candidate given id', (done) => {
-  //       chai.request(server)
-  //         .get('/api/candidate/' + 1)
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.companies.should.be.a('array');
-  //           res.body.should.have.property('firstName');
-  //             res.body.should.have.property('lastName');
-  //             done();
-  //         });
-  //     });
-  //   });
-
-
   describe('/POST Register new user', () => {
     it('it should register new user', (done) => {
       let user = {
         "firstName": "Jonny",
         "lastName": "SilverHand",
-        "email": "jonnt12@email.com",
+        "email": "jonnt123@email.com",
         "password": "jonny"
       }
       chai.request(server)
@@ -98,52 +73,6 @@ describe('USER TEST REQUESTS', () => {
     });
   });
 
-  //   describe('/PUT/:id candidate', () => {
-  //     it('it should UPDATE a candidate given the id', (done) => {
-  //       chai.request(server)
-  //         .put('/api/candidate/' + 1)
-  //         .send({
-  //           "firstName": "Jony1",
-  //           "lastName": "Last name updated!",
-  //           "companies": ["grd", "Veoneer"],
-  //           "technologies": ["NodeJsssss", "JavaScript"]
-  //         })
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('object');
-  //           res.body.should.have.property('lastName').eql("Last name updated!");
 
-  //           done();
-  //         });
-  //     });
-  //     it('it should UPDATE a candidate given the id with new companies', (done) => {
-  //       chai.request(server)
-  //         .put('/api/candidate/' + 1)
-  //         .send({
-  //           "firstName": "Jony1",
-  //           "lastName": "Last name updated!",
-  //           "companies": ["new_company", "new_company"],
-  //           "technologies": ["NodeJsssss", "JavaScript"]
-  //         })
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('object');
-  //           res.body.should.have.property('lastName').eql("Last name updated!");
-
-  //           done();
-  //         });
-  //     });
-  //     it('it should FAIL UPDATE a candidate given the id', (done) => {
-  //       chai.request(server)
-  //         .put('/api/candidate/' + 1)
-  //         .send({
-  //           "firstName": undefined,
-  //         })
-  //         .end((err, res) => {
-  //           res.should.have.status(400);
-  //           done();
-  //         });
-  //     });
-  //   });
 
 });
